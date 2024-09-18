905048842614b4adc6b5587dd5dc84336554f454
Abhinav7279 <abhinavanand7279@gmail.com>
2024-09-19 02:35:09 +0530

    initial update

---
+++ Array/JS_PQ4.js
@@ -0,0 +1,15 @@
+//Q create an array to store companies "bloomberg","Microsoft","uber","google","ibm","netflix"
+// a  remove the first comapny from the array
+
+// b remove uber  and add ola in its place 
+
+// c add amazon at the end 
+ let companies  = [ "bloomberg","Microsoft","uber","google","ibm","netflix"];
+  // ans a 
+  //companies.shift("bloomberg")
+
+  //ans b 
+  //companies.splice(2,1,"ola")
+
+// ans c 
+companies.push("Amamzon")
---
+++ Array/methods.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4,5,6,7,];
+//arr.splice(2,2,101,102);
+
+//add element to splice methods
+//arr.splice(2,0,101)
+
+// delete element
+arr.splice(3,1)
+
+// Replace element
+arr.splice(3,1, 101)
