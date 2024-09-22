b168a1fa2d5fa8b62f455e328a5b689639860597
Abhinav7279 <abhinavanand7279@gmail.com>
2024-09-23 03:07:39 +0530

    add super keyword

--- classes/app.js
+++ classes/app.js
@@ -5,3 +5,49 @@ const student = {
         console.log("Marks = ", this.marks);
     }
 };
+
+class toyotaCar{
+    start (){
+        console.log("start")
+    }
+    stop (){
+        console.log("stop")
+    }
+    setBrand(brand){
+        this.brand = brand;
+    }
+}
+
+// super keyword
+
+class Parent {
+    constructor(name){
+        this.species = "homo sapiens";
+       this.name =name;
+    }
+    eat(){
+        console.log("eat");
+    }
+    sleep(){
+        console.log("sleep");
+    }
+}
+
+class engineer extends Parent{
+    constructor(name){
+        super(name);
+    }
+    
+    work(){
+       super.eat();
+        console.log("it also do some work ");
+    }
+}
+
+let fortuner = new toyotaCar();
+fortuner.setBrand("fortuner");
+let lexus = new toyotaCar();
+lexus.setBrand("lexus");
+
+// super keyword
+let engObj = new engineer("Abhinav");
